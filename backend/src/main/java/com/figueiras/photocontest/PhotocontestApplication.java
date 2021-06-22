5d1969573238d1b422a94f83fc27e03fb37c9719
Jose Figueiras <jose.figueirasm@udc.es>
2021-06-23 00:49:34 +0200

    Añadida estructura del backend
    Además se corrige el README actualizándolo.

---
+++ backend/src/main/java/com/figueiras/photocontest/PhotocontestApplication.java
@@ -0,0 +1,13 @@
+package com.figueiras.photocontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotocontestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotocontestApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/figueiras/photocontest/PhotocontestApplicationTests.java
@@ -0,0 +1,10 @@
+package com.figueiras.photocontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotocontestApplicationTests {
+
+
+}

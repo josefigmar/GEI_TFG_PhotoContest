2b09e2d1c94bdad9b6e4208a33a930e7c608982e
Jose Figueiras <josefigueirasm@protonmail.com>
2021-07-21 16:44:23 +0200

    Cambio en CU buscar usuarios
    Por defecto, al seleccionar el botón usuarios, se cargan los usuarios ordenados por nombre de usuario para que la página no se vea tan vacía.

--- frontend/src/modules/user/components/FindUsers.js
+++ frontend/src/modules/user/components/FindUsers.js
@@ -1,4 +1,4 @@
-import { useState} from 'react';
+import { useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import { FormattedMessage } from 'react-intl';
 import FindUsersResult from './FindUsersResult';
@@ -9,15 +9,32 @@ const FindUsers = () => {
     const [nombreUsuario, setNombreUsuario] = useState("");
     const [findUsersResult, setFindUsersResult] = useState("");
 
-    const paginationSize = 4;
+    const paginationSize = 5;
 
     const handleSubmit = event => {
         event.preventDefault();
         backend.userService.findUsers(
-            {   nombre: nombreUsuario.trim(),
+            {
+                nombre: nombreUsuario.trim(),
                 page: 0,
-                size: paginationSize}, result => setFindUsersResult({result, nombre: nombreUsuario.trim(), page: 0, size: paginationSize}));
+                size: paginationSize
+            }, result => setFindUsersResult({ result, nombre: nombreUsuario.trim(), page: 0, size: paginationSize }));
     }
+
+    // Por defecto, se cargan los usuarios ordenados alfabéticamente por nombre de usuario
+    // para que la apariencia de la página no sea tan vacía
+    useEffect(() => {
+        backend.userService.findUsers(
+            {
+                nombre: "",
+                page: 0,
+                size: paginationSize
+            }, result => setFindUsersResult({
+                result, nombre: "", page: 0, size: paginationSize
+            }
+            ));
+        // eslint-disable-next-line
+    }, [])
 
     return (
 
@@ -25,21 +42,21 @@ const FindUsers = () => {
             <div className="d-flex justify-content-center">
                 <form className="form-inline mt-2 mt-md-0" onSubmit={e => handleSubmit(e)}>
                     <FormattedMessage id='user.FindUsers.PlaceHolder'>
-                        {placeholder => <input id="keywords" placeholder={placeholder} type="text" className="form-control mr-sm-2" value={nombreUsuario} onChange={e => setNombreUsuario(e.target.value)}/>}
+                        {placeholder => <input id="keywords" placeholder={placeholder} type="text" className="form-control mr-sm-2" value={nombreUsuario} onChange={e => setNombreUsuario(e.target.value)} />}
                     </FormattedMessage>
-                   
+
                     <button placeholder="" type="submit" className="btn btn-primary my-2 my-sm-0" onSubmit={e => setNombreUsuario(e.target.value)}>
-                        <FormattedMessage id='global.buttons.search'/>
+                        <FormattedMessage id='global.buttons.search' />
                     </button>
                 </form>
-                
+
 
             </div>
-            <br/>
-            <br/>
-            <br/>
+            <br />
+            <br />
+            <br />
             <div>
-                <FindUsersResult findUsersResult={findUsersResult} updateFindUsersResult={setFindUsersResult}/>
+                <FindUsersResult findUsersResult={findUsersResult} updateFindUsersResult={setFindUsersResult} />
             </div>
         </Container>
 

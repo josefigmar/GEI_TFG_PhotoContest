35b2a84b3999c7fafc30de30b9b37fbe5e5e86b7
Jose Figueiras <josefigueirasm@protonmail.com>
2021-08-03 22:55:34 +0200

    Corregido bug en la cuenta atrás
    * El bug hacía que cuando el contador llegara a 0 en vez de quedarse en 0 contabilizaba la resta entre las fechas como positiva y seguía mostrando una cuenta atrás que carecía de sentido alguno.

--- frontend/src/modules/contest/components/ContestPreparationStatus.js
+++ frontend/src/modules/contest/components/ContestPreparationStatus.js
@@ -6,14 +6,24 @@ import { detailed } from "yargs-parser";
 
 const ContestPreparationStatus = ({ contestData }) => {
 
+    const calculateDiffBetweenDates = (date2, date1) => {
+
+        let diff = date2 - date1;
+        if (diff > 0) {
+            return diff;
+        } else {
+            return 0;
+        }
+    }
+
     return (
         <div>
             <ContestHeader contestData={contestData} />
             <hr />
             <Container className="centering">
-                <h4><FormattedMessage id='contest.contestDetail.Body.PreparationState.CountDown.Title'/></h4>
+                <h4><FormattedMessage id='contest.contestDetail.Body.PreparationState.CountDown.Title' /></h4>
                 <Spinner animation="grow" variant="danger" size="sm" />&ensp;
-                <h4 className="hWithoutLineBreak"><Countdown daysInHours={true} date={Date.now() + Math.abs(new Date(contestData.fechaInicio) - Date.now())} /></h4>
+                <h4 className="hWithoutLineBreak"><Countdown daysInHours={true} date={Date.now() + calculateDiffBetweenDates(new Date(contestData.fechaInicio), Date.now())} /></h4>
             </Container>
         </div>
     )
